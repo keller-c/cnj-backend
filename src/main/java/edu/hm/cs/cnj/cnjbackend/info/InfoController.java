085aa84983e7abfd98857f21a9338a526e5c834f
Christian Keller <christian@fam-kellerclan.de>
2017-11-14 13:24:43 +0100

    Praktikum 7.11.

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,19 +1,18 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
-
-    @Value("${cnj.info.message}")
-    private String message;
+    @Autowired
+    InfoProperties properties;
 
     @RequestMapping("/info")
-    public Info info() {
+    public Info getInfo() {
         Info result = new Info();
-        result.setMessage(message);
+        result.setMessage(properties.getMessage());
         return result;
     }
 }
